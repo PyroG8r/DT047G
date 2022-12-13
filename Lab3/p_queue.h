f4a1c01fe2e87ddca5332b3dbee2f55572340bfd
Emil Jons <emil.jons47@gmail.com>
2022-12-13 11:50:31 +0100

    lab3

--- Lab3/p_queue.h
+++ Lab3/p_queue.h
@@ -6,11 +6,17 @@
 #ifndef LAB3_P_QUEUE_H
 #define LAB3_P_QUEUE_H
 
-
 #include <iostream>
 #include <vector>
 
-template <typename T, typename Comp>
+/*
+template <typename T>
+bool less(const T &a, const T &b)  {
+    return a < b;
+}
+*/
+
+template <typename T, typename Comp = std::less<>>
 class p_queue {
 public:
     p_queue() = default;
@@ -22,8 +28,11 @@ public:
     }
 
     void push(T orderInstance) {
-        auto pos = std::find_if(pq.begin(), pq.end(), less(orderInstance));
-        pq.insert(pos, orderInstance);
+
+        //using comp function, insert orderInstance in the first position where it is less than the element at that position
+        auto it = std::find_if(pq.begin(), pq.end(), [&](const T &element) { return comp(orderInstance, element); });
+        pq.insert(it, orderInstance);
+
     }
     int size() {
         return pq.size();
@@ -33,16 +42,8 @@ public:
     }
 private:
     std::vector<T> pq;
+    Comp comp;
 
-    struct less{
-        less(const T& value): value(value) {}
-
-        bool operator()(const T& e) const{
-            Comp comp;
-            return comp(value, e);
-        }
-        T value;
-    };
 };
 
 #endif //LAB3_P_QUEUE_H
